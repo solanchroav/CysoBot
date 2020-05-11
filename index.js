49cc2534ef2a89982c9a73ece43977e2a441abe6
Solanch Roa <sols.rou14@gmail.com>
2020-05-10 23:38:39 -0300

    v1.0.0 commit-2

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var request = require('request');
+'use strict'
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
 
 const APP_TOKEN = 'EAAJqlkCtRg8BAMmmHyM4jd456vHxr1rJHKIV0A8oa42tLrHsWgt6u8or9rnrrWMhKJJhKNSMTZBz0jKB1I2eL7FZCRpbOZCedq8IOyg1V7DpateMuZBQLXYwrmaXL9bLugy7CgQzB4rWY5xlhlGCeDky6wnlEDkKpSW9qN7OXQZDZD';
 
@@ -27,3 +29,46 @@ app.get('/webhook', function (req, res) {
         res.send('No tienes permitido el acceso a esta ruta');
     }
 }); 
+
+app.post('/webhook', function(req, res){
+    const webhook_event = req.body.entry[0];
+    if(webhook_event.messaging) {
+        webhook_event.messaging.forEach(event => {
+            handleMessage(event);
+        })
+    }
+    res.sendStatus(200);
+})
+
+function handleMessage(event){
+    const senderId = event.sender.id;
+    const messageText = event.message.text;
+    const messageData = {
+        recipient: {
+            id: senderId
+        },
+        message: {
+            text: messageText
+        }
+    }
+    callSendApi(messageData);
+}
+
+function callSendApi(response) {
+    request({
+        "uri": "https://graph.facebook.com/me/messages",
+        "qs": {
+            "access_token": APP_TOKEN
+        },
+        "method": "POST",
+        "json": response
+    }
+    // function(err) {
+    //     if(err) {
+    //         console.log('Ha ocurrido un error')
+    //     } else {
+    //         console.log('Mensaje enviado')
+    //     }
+    // }
+)
+}
